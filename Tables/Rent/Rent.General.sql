63c64902f77fb04837f58610ce9998fc9eb493ae
manish0071 <test_pop@outlook.com>
2018-08-24 23:28:35 +0530

    Rent.General

---
+++ Tables/Rent/Rent.General.sql
@@ -0,0 +1,44 @@
+USE [Go-Kart]
+GO
+
+/****** Object:  Table [Rent].[General]    Script Date: 24-08-2018 23:27:09 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [Rent].[General](
+	[ItemId] [char](10) NOT NULL,
+	[ItemDesc] [nchar](100) NOT NULL,
+	[ItemTitle] [nchar](50) NOT NULL,
+	[ItemBlob] [nchar](10) NULL,
+	[UserId] [nchar](10) NOT NULL,
+	[ItemType] [nchar](20) NOT NULL,
+	[SecurityDeposit] [money] NULL,
+	[DateAdded] [datetime] NOT NULL,
+	[WarrantyPeriod_M] [nchar](10) NULL,
+	[RentId] [nchar](10) NOT NULL,
+	[WarrantyPeriod_Y] [nchar](10) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ItemId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [Rent].[General] ADD  DEFAULT ((0)) FOR [WarrantyPeriod_M]
+GO
+
+ALTER TABLE [Rent].[General] ADD  DEFAULT ((0)) FOR [WarrantyPeriod_Y]
+GO
+
+ALTER TABLE [Rent].[General]  WITH CHECK ADD  CONSTRAINT [FK_General_ToUser] FOREIGN KEY([UserId])
+REFERENCES [dbo].[User] ([UserId])
+GO
+
+ALTER TABLE [Rent].[General] CHECK CONSTRAINT [FK_General_ToUser]
+GO
+
+
